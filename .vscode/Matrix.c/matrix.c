3801225e78e252471d60dea9a6c809e2741b1c08
Shashank <shashanksaxena226@gmail.com>
2021-12-30 15:01:40 +0530

    k

---
+++ .vscode/Matrix.c/matrix.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int a[3][3], b[3][3], c[3][3];
+int main()
+{
+    int inp(int[][], int, int);
+    int oper(int[][], int, int);
+    int out(int[][], int, int);
+    return (0);
+}
