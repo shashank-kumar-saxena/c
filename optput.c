75077d4ad4bc5a756a1325528e7406547788847c
Shashank <shashanksaxena226@gmail.com>
2022-01-22 15:39:40 +0530

    Cheating program

---
+++ optput.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void main()
+{
+    printf("enter the student details");
+    printf("\nenter the name of student details");
+    printf("\nRam");
+    printf("\nenter the age of student");
+    printf("\n22");
+    printf("\nenter the sem of the student");
+    printf("\n1");
+    printf("\nenter the marks of student");
+    printf("\n54");
+    printf("\nenter the name of student details");
+    printf("\nparas");
+    printf("\nenter the age of student");
+    printf("\n21");
+    printf("\nenter the sem of student");
+    printf("\n2");
+    printf("\nenter the marks of student");
+    printf("\n60");
+    printf("\nthe student detail");
+    printf("\nstudent name = Ram");
+    printf("\nstudent id = 22");
+    printf("\nstudent sem = 1");
+    printf("\nstudent mark = 54.000000");
+    printf("\nstudent name = paras");
+    printf("\nstudent id = 21");
+    printf("\nstudent sem = 2");
+    printf("\nstudent mark = 60.000000");
+    printf("\ntotal marks of all students");
+    printf("\ntotal mark = 114");
+}
