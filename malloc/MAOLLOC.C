e75bc31aeacf140b671f16b71f4dfff39ec2663d
Shashank Kumar <69252042+shashank-kumar-saxena@users.noreply.github.com>
2022-02-14 11:41:00 +0530

    Add files via upload

---
+++ malloc/MAOLLOC.C
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+clscr();
+{
+int a,b,area;
+printf("enter the lenght of rectangle");
+scanf("%d%d",&a &b);
+area=a*b
+printf("the area of rectangle=d",area);
+getch();
+
+}
