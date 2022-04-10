5760973981256c25021f4acc4242862bbb3168ce
Shashank <shashanksaxena226@gmail.com>
2022-04-10 20:10:56 +0530

    changes

--- c++/removespace.cpp
+++ c++/removespace.cpp
@@ -7,20 +7,19 @@ int main()
     string st = "", str = "";
     int p = 1;
     cin >> s;
-    s = " " + s;
-    // for (int i = s.length() - 1; i >= 0; i--)
-    // {
-    //     if (s[i] != " ")
-    //     {
-    //         st = s[i];
-    //         p = 1;
-    //     }
-    //     else if (s == " " && p == 1)
-    //     {
-    //         st = st + s[i];
-    //         p = 0;
-    //     }
-    // }
+    for (int i = s.length() - 1; i >= 0; i--)
+    {
+        if (s[i] != " ")
+        {
+            st = s[i];
+            p = 1;
+        }
+        else if (s == " " && p == 1)
+        {
+            st = st + s[i];
+            p = 0;
+        }
+    }
     cout << s;
     return 0;
 }
